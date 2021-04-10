78996bf114aab3d8b0a4b76290a631b549ccb7cc
metehankoc <59233358+metehankoc@users.noreply.github.com>
2021-04-10 18:09:36 +0300

    optimization
    -enemy
    -moving platform
    -spawner
    -bullet

--- src/scripts/JumpLocation.gd
+++ src/scripts/JumpLocation.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+onready var animated_sprite = $AnimatedSprite
+
 func _ready():	
 	var character = get_tree().get_root().get_child(1).get_node("Char")
 	if character != null:
@@ -9,11 +11,10 @@ func _physics_process(delta):
 	
 	var position = get_tree().get_root().get_child(1).get_node("Char")._get_next_position()
 	if position == self.position:
-		$AnimatedSprite.modulate = Color(255,0,0)
-		$AnimatedSprite.play("next")
+		animated_sprite.modulate = Color(255,0,0)
+		animated_sprite.play("next")
 	else:
-		$AnimatedSprite.modulate = Color(255,255,255)
-		$AnimatedSprite.play("idle")
-	
+		animated_sprite.modulate = Color(255,255,255)
+		animated_sprite.play("idle")
 	pass
 
