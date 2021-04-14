0c4a037b55f8b8d3b8a2df445db816884f9bd3a5
alkimonen <alkimonen@hotmail.com>
2021-04-14 19:56:27 +0300

    Background motion offset optimization

--- src/scripts/Background.gd
+++ src/scripts/Background.gd
@@ -1,10 +1,15 @@
 extends ParallaxBackground
 
+export(Vector2) var back_layer_speed = Vector2(-10, -4)
+export(Vector2) var mid_layer_speed = Vector2(-20, -4)
+export(Vector2) var front_layer_speed = Vector2(-30, -4)
+
+
 const backspeed = 0.1
 const middlespeed = 0.2
 const frontspeed = 0.3
 
-func _process(_delta):
-	$ParallaxLayerBack.motion_offset.x -= backspeed
-	$ParallaxLayerMid.motion_offset.x -= middlespeed
-	$ParallaxLayerFront.motion_offset.x -= frontspeed
+func _process(delta):
+	$ParallaxLayerBack.motion_offset += back_layer_speed * delta
+	$ParallaxLayerMid.motion_offset += mid_layer_speed * delta
+	$ParallaxLayerFront.motion_offset += front_layer_speed * delta
