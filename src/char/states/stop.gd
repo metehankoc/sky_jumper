de180bb74b82c44c5c3425f5d7044a44915c0f1c
alkimonen <alkimonen@hotmail.com>
2021-04-21 22:58:11 +0300

    File system reorganization

---
+++ src/char/states/stop.gd
@@ -0,0 +1,15 @@
+extends "../State.gd"
+
+onready var animatedSprite = owner.get_node("AnimatedSprite")
+
+func _enter():
+	owner.move_and_slide(Vector2(0,0))
+	animatedSprite.stop()
+
+
+func _exit():
+	pass
+
+
+func _update(_delta):
+	pass
